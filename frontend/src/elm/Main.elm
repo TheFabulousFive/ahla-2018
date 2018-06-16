e4e936ed0f608acd501f2ac59c0b3ca0408ca139
David Nuon <david@davidnuon.com>
2018-06-15 22:52:33 -0700

    Added a websockets message

--- frontend/src/elm/Main.elm
+++ frontend/src/elm/Main.elm
@@ -3,13 +3,16 @@ module Main exposing (..)
 import Html exposing (..)
 
 import Keyboard
+import WebSocket
+
 
 type alias Model =
     {
         currentSlide : Int,
         counter: Int,
         mouseX: Int, 
-        mouseY: Int
+        mouseY: Int,
+        message: String
     }
 
 type Msg
@@ -17,9 +20,10 @@ type Msg
     | Decrement
     | KeyMsg Keyboard.KeyCode
     | MouseMovement Int Int
+    | WSMessage String
 
 initState : Model 
-initState = { currentSlide = 0, counter = 0, mouseX = 0, mouseY = 0 }
+initState = { currentSlide = 0, counter = 0, mouseX = 0, mouseY = 0, message = "" }
 
 init : ( Model, Cmd Msg )
 init =
@@ -27,15 +31,22 @@ init =
 
 -- Model updates here
 update : Msg -> Model -> ( Model, Cmd msg )
-update msg model = ( model, Cmd.none )
+update msg model = 
+    case msg of
+        WSMessage ws_msg ->
+            ( { model | message = ws_msg } , Cmd.none )
+        _ ->
+            ( model, Cmd.none )
 
 -- Put websockets subscriotions here
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model = Sub.batch [
+    WebSocket.listen "ws://localhost:8081/" WSMessage
+    ]
 
 -- View code goes here
 view : Model -> Html Msg
-view model = div [] [text "what is this"]
+view model = div [] [text "what is this", text model.message ]
 
 -- Entry point
 main : Program Never Model Msg
